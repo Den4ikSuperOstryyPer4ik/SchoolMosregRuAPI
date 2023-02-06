38a3afc05f3c79fdb160c0db50e40647476eac38
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-02-06 12:27:41 +0300

    Update Subjects.py

--- school_mosreg_api/types/Subjects.py
+++ school_mosreg_api/types/Subjects.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from .model import Type
 
-class Subject(BaseModel):
+class Subject(Type):
     """[GET] /v2.0/edu-groups/{eduGroup}/subjects\n~~~\n/v2.0/schools/{school}/subjects\n~~~\nСписок предметов, преподаваемых в классе в текущем отчётном периоде или преподаваемых в образовательной организации в текущем учебном году\n~~~\nПрава доступа: EducationalInfo\n~~~"""
     
     id: int
