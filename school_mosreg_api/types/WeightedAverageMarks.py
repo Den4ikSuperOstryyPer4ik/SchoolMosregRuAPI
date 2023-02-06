4523e4a7f0173c775e751ffee4d8d3fa6448a506
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-02-06 12:27:23 +0300

    Update WeightedAverageMarks.py

--- school_mosreg_api/types/WeightedAverageMarks.py
+++ school_mosreg_api/types/WeightedAverageMarks.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel
+from .model import Type
 
-class WeightedAverageMarks_SubjectMark_Mark(BaseModel):
+class WeightedAverageMarks_SubjectMark_Mark(Type):
     person: int
     value: int
 
-class WeightedAverageMarks_SubjectMarks(BaseModel):
+class WeightedAverageMarks_SubjectMarks(Type):
     subject: int
     subject_str: str
     marks: list[WeightedAverageMarks_SubjectMark_Mark]
 
-class WeightedAverageMarks(BaseModel):
+class WeightedAverageMarks(Type):
     """[GET] /v2.0/edu-groups/{group}/wa-marks/{from}/{to}\n~~~\nПрава доступа: EducationalInfo\n~~~"""
     
     subjectMarks: list[WeightedAverageMarks_SubjectMarks]
