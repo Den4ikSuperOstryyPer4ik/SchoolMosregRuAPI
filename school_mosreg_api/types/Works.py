eb65da560ce5438e6aea545a0afca59284e78226
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-02-06 12:27:22 +0300

    Update Works.py

--- school_mosreg_api/types/Works.py
+++ school_mosreg_api/types/Works.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from .model import Type
 
 from datetime import datetime
 
-class Work_Task(BaseModel):
+class Work_Task(Type):
     id: int
     id_str: str
     person: int
@@ -13,7 +13,7 @@ class Work_Task(BaseModel):
     targetDate: datetime
 
 
-class Work(BaseModel):
+class Work(Type):
     """[GET/POST/DELETE/PUT]\n~~~\n/v2.0/works\n~~~\n/v2.0/lessons/{lesson}/works\n~~~\n/v2.0/works/{work}\n~~~\n/v2.0/works/many\n~~~\nПолучение списка всех типов работ школы (Данный класс - 1 тип работы)\n~~~\nПрава доступа: EducationalInfo\n~~~"""
     
     id: int
@@ -29,7 +29,7 @@ class Work(BaseModel):
     eduGroup: int
     eduGroup_str: str
     tasks: list[Work_Task] | list = []
-    text: str | bytes
+    text: str | bytes | None = None
     periodNumber: int
     periodType: str
     subjectId: int
@@ -37,11 +37,11 @@ class Work(BaseModel):
     targetDate: datetime
     sentDate: datetime = None
     createdBy: int
-    files: list[int] = []
-    oneDriveLinks: list[int] = []
+    files: list[int] | None = []
+    oneDriveLinks: list[int] | None = []
 
 
-class EditStatusHomeWork(BaseModel):
+class EditStatusHomeWork(Type):
     """[POST]\n~~~\n/v2.0/works/{work}/persons/{person}/status\n~~~\nИзменить статус выполнения домашней работы учащимся.\n~~~\nПрава доступа: EducationalInfo\n~~~"""
     
     id: int
