54873e869290bc7e504561af904303e49c9fc47d
Den4ikSuperOstryyPer4ik <oleynik.denis.2009@gmail.com>
2023-02-06 12:27:09 +0300

    Update MarkHistograms.py

--- school_mosreg_api/types/MarkHistograms.py
+++ school_mosreg_api/types/MarkHistograms.py
@@ -1,29 +1,29 @@
-from pydantic import BaseModel
+from .model import Type
 
-class markNumbers_mark(BaseModel):
+class markNumbers_mark(Type):
     value: str
     count: int
 
 
-class markNumbers(BaseModel):
+class markNumbers(Type):
     markNumber: int
     marks: list[markNumbers_mark]
 
 
-class MarksHistogram(BaseModel):
+class MarksHistogram(Type):
     """Деперсонализированная гистограма оценок всего класса"""
 
 
     markNumbers: list[markNumbers]
 
 
-class WORK(BaseModel):
+class WORK(Type):
     workId: int
     workId_str: str
     markNumbers: list[markNumbers]
 
 
-class MarksHistogramByPeriod(BaseModel):
+class MarksHistogramByPeriod(Type):
     """Деперсонализированная гистограма оценок всего класса за отчетный период"""
     
     works: list[WORK]
